aa32c5f6568658ffe690acd714e8e746172a8532
Brandon Cullifer <brandon.cullifer@gmail.com>
2026-06-18 13:22:39 -0400

    deploy script

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/VRSW/',
   plugins: [react()],
+  base: '/VRSW/'
   server: {
     port: 5173,
     open: true
